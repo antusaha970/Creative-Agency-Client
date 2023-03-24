21049f1c9475aca39e365e5e2e467210c8cc528f
antu <antusaha970@gmail.com>
2023-03-24 20:16:10 +0600

    customer sidebar coded

--- src/DashboardPage/SideBar/SideBar.js
+++ src/DashboardPage/SideBar/SideBar.js
@@ -1,10 +1,43 @@
-import { Grid } from '@mui/material';
+import { ContentCut } from '@mui/icons-material';
+import { Grid, ListItemIcon, ListItemText, MenuItem, MenuList, Stack, Typography } from '@mui/material';
 import React from 'react';
-
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import RateReviewOutlinedIcon from '@mui/icons-material/RateReviewOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import { Link } from 'react-router-dom';
+import './SideBar.css';
 const SideBar = () => {
     return (
         <Grid item lg={2} md={2} sm={2} xs={12}>
-            sidebar
+            <MenuList>
+                <Link to='/dashboard/order'>
+                    <MenuItem>
+                        <ListItemIcon>
+                            <ShoppingCartOutlinedIcon fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText>Order</ListItemText>
+                    </MenuItem>
+                </Link>
+
+                <Link to='/dashboard/serviceList'>
+                    <MenuItem>
+                        <ListItemIcon>
+                            <SettingsOutlinedIcon fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText>Service List</ListItemText>
+                    </MenuItem>
+                </Link>
+
+                <Link to='/dashboard/review'>
+                    <MenuItem>
+                        <ListItemIcon>
+                            <RateReviewOutlinedIcon fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText>Review</ListItemText>
+                    </MenuItem>
+                </Link>
+
+            </MenuList>
         </Grid>
     );
 };
